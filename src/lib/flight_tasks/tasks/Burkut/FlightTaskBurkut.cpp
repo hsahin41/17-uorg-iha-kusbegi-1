53eab5b66b9c3a4092a9a7ace8d60afef7c0266e
İsmail Emre Canpınar <44278499+iemre98@users.noreply.github.com>
2020-02-22 12:38:20 +0300

    Add files via upload
    Burkut flighttask

---
+++ src/lib/flight_tasks/tasks/Burkut/FlightTaskBurkut.cpp
@@ -0,0 +1,242 @@
+#include "FlightTaskBurkut.hpp"
+
+
+using namespace matrix;
+
+/*
+KULLANISLI FONKSIYONLAR
+
+uORB::Publication<vehicle_land_detected_s> _vehicle_land_detected_pub{ORB_ID(vehicle_land_detected)};
+uORB::Publication<position_setpoint_s>	_position_setpoint_pub{ORB_ID(position_setpoint)};
+
+*/
+
+void FlightTaskBurkut::_publishVehicleCmdDoLand()
+{
+	vehicle_command_s command{};
+	command.timestamp = hrt_absolute_time();
+	command.command = vehicle_command_s::VEHICLE_CMD_DO_SET_MODE;
+	command.param1 = (float)1; // base mode
+	command.param3 = (float)0; // sub mode
+	command.target_system = 1;
+	command.target_component = 1;
+	command.source_system = 1;
+	command.source_component = 1;
+	command.confirmation = false;
+	command.from_external = false;
+	command.param2 = (float)PX4_CUSTOM_MAIN_MODE_AUTO;
+	command.param3 = (float)PX4_CUSTOM_SUB_MODE_AUTO_LAND;
+
+	// publish the vehicle command
+	_pub_vehicle_command.publish(command);
+
+	_param_mpc_auto_mode.set(_default_mpc_auto_mode);
+	_param_mpc_auto_mode.commit();
+	updateParams();
+
+}
+void FlightTaskBurkut::_bodyToNedFrame(float xBody,float yBody,float yawBody)
+{
+	//translate body frame to NED frame and publish position setpoints.
+	_position_setpoint(0) = _origin_x + ( xBody * cosf(yawBody) ) - ( yBody * sinf(yawBody) );
+	_position_setpoint(1) = _origin_y + ( xBody * sinf(yawBody) ) + ( yBody * cosf(yawBody) );
+}
+
+
+bool FlightTaskBurkut::activate(vehicle_local_position_setpoint_s last_setpoint)
+{
+
+	bool ret = FlightTask::activate(last_setpoint);
+
+	_position_setpoint(0) = _position(0);
+	_position_setpoint(1) = _position(1);
+	_position_setpoint(2) = _position(2);
+	_yaw_setpoint = _yaw;
+
+
+	// Pozisyonlar hafızaya alınır.
+	_origin_z = _position(2);// 2 Metre _origin_z ile tutulur.
+	_origin_y = _position(1);// x,y = 0,0 konumları + yaw açısı saklanır.
+	_origin_x = _position(0);
+	_origin_yaw = _yaw;
+
+	//yaw speed setted.
+	_yaw_speed = 320;//_param_yaw_speed.get();
+
+
+	return ret;
+
+}
+
+
+bool FlightTaskBurkut::update()
+{
+
+	// Update önce stage durumunu kontrol edicek.Önce stage-0 ile başlayacak.
+	//stage-0 drone 2 metre yüksekte olduğu konum.
+	//Burkut 2 metreye manuel yada qgc tarafından çıakrılıcak sonrasında parametre aktif edilecek.
+
+	//_stage = 0 by default.
+
+	switch (_stage)
+	{
+	case 0:
+
+		//5 metre ileri giderken dönme işlemi başlar.
+
+		_counter = _counter + _counter_speed; // adımı büyütüyoruz
+		//_position_setpoint(0) = _origin_x + _counter;
+		// büyütülen adımı güncelliyoruz.
+		_bodyToNedFrame((_counter),0.0f,_origin_yaw); // çevir ve yayınla
+		_yaw_setpoint = _yaw_speed * _counter * 3.141592653589793f / 180.f; //dönüş hareketi yaptırıyoruz.
+
+		//görev aşaması başarı ile tamamlandı ise
+		//stage değişimi öncesi hazırlık ve stage değişimi yapılır.
+		if(  _counter  >= 5.0f   )
+		{
+			_yaw_setpoint = _origin_yaw;// yönümüzü orjinal yöne eşitleriz.
+			_stage = 1; //stage-1 e geçilir.
+			//stage değiştiği için counter sıfırlanır ki sonraki görevde tekrar kullanılabilsin.
+			_counter = 0.0f;//counter sıfırlanır.
+
+
+			//sonra hepsi sonraki görev için hafızaya alınır.
+			_origin_x = _origin_x + (5.0f * cosf(_origin_yaw));
+			_origin_y = _origin_y + (5.0f * sinf(_origin_yaw));
+			// x ekseni 5 metre taşınır.Diğer eksenlerde oynama yok.
+			//bu sebeple diğerleri değiştirilmez.
+
+
+			// ***********  alınan origin => x,y,z = 5,0,2 / yaw = 0 **********
+
+
+		}
+
+
+		break;
+	case 1:
+		_yaw_setpoint = _origin_yaw;// yönümüzü orjinal yöne eşitleriz.
+
+		// 5x5 hipotenüs geçilecek.
+		// 5 metre geri ve 5 metre yukarı adımlarından oluşacak bir stage.
+		//counter 0 dan başlayarak 5 e kadar artıcak iki eksen içinde negatif olucak şekilde
+		//setpointe eklencek.Bu sayede smooth bir hareket sağlanacak.
+		//bir önceki stage de counter sıfırlanmıştı.Sorunsuz şekilde devam edebiliriz.
+		_counter = _counter + _counter_speed; // adımı büyütüyoruz
+
+		//_position_setpoint(0) = _origin_x - _counter;
+		// 5 metre geri
+		_bodyToNedFrame((-_counter),0.0f,_origin_yaw);
+		//_position_setpoint(1) = _origin_y;
+		//y ekseni her zaman aynı origin_y değerine eşit.Ve bu değer değişmemeli.
+		_position_setpoint(2) = _origin_z - _counter;
+		// 5 metre yukarı
+
+		//5 metre olunca ve stage-2 geçeceğiz.
+
+
+		if( _counter >= 5.0f )
+		{
+
+			//sonra hepsi sonraki görev için hafızaya alınır.
+			_origin_z = _origin_z - 5.0f; //-2 metreyi -5 ekleyerek -7 yaptık (NED FRAME)
+			_origin_x = _origin_x - (5.0f * cosf(_origin_yaw));
+			_origin_y = _origin_y - (5.0f * sinf(_origin_yaw));
+			// 5 metreden 5 çıkarak 0 yaptık.
+			//y ekseni her zamanki gibi sabit.
+			//counter sıfırlanmalı sonraki görev için.
+			_counter = 0.0f;
+			// ************* alınan origin => x,y,z = 0,0,7 ***************
+			_stage = 2;
+
+		}
+
+		break;
+	case 2:
+		_counter = _counter + _counter_speed; // z ekseninde 5 metre aşağı inme adımları
+		_position_setpoint(2) = _origin_z + _counter;
+		// adımları + olarak ekliyoruz çünkü şuan değer -7 , -7+5 = -2 (hedef 2m NED FRAME)
+		_yaw_setpoint = _yaw_speed * _counter * 3.141592653589793f / 180.f; //dönüş hareketi yaptırıyoruz.
+
+		if(  _counter  >= 5.0f   )
+		{
+
+			//sonra hepsi sonraki görev için hafızaya alınır.
+			_origin_z = _origin_z + 5.0f;
+			//y ekseni korunur
+			//x eksen, korunur
+
+			// ************* alınan origin => x,y,z = 0,0,2 ***************
+			_counter = 0.0f; // counterın sıfırlandığını kontrol edip sonraki stage i öyle başlatıyoruz.
+			_stage = 3; // daire çizmeye başla , stage-3 geçilsin.
+			//sonraki görev için gerekli parametreler.Loop değil activate gibi
+			//çalışması için burdaki geçiş koşulunun içerisine yazılır.
+			//her loop başlangıcında bir değeri tekrar atamak zorunda kalmasın diye.
+			_counter_speed = (_counter_speed * 10.0f);//artık derece olduğu için 10 kat hızlandırıyoruz.
+			_yaw_speed = (_yaw_speed / 10.0f); //yaw hızıda buna bağlı yavaşlatılır.(10kat)
+		}
+
+		break;
+	case 3:
+		// alttaki 2 satır bir önceki stage de yazılıcağından burda commentout edildi.
+		//_counter_speed = 0.1f;//artık derece olduğu için 10 kat hızlandırıyoruz.
+		//_yaw_speed = 30.0f; //yaw hızıda buna bağlı yavaşlatılır.
+
+		_counter = _counter + _counter_speed;
+
+		//convertin degree to radian
+		_radian_of_degree = _counter * 3.141592653589793f / 180.f;
+
+		//setting new setpoints
+		//_position_setpoint(0) = _origin_x + (_radius_of_chamber * sinf(_radian_of_degree));
+		_bodyToNedFrame( (_radius_of_chamber * sinf(_radian_of_degree)) , 0.0f,_origin_yaw);
+		_position_setpoint(2) = _origin_z - 3.0f + (_radius_of_chamber * cosf(_radian_of_degree));
+		_yaw_setpoint = _yaw_speed * _counter * 3.141592653589793f / 180.f;
+
+		if(_counter >= 360.0f)
+		{
+			//görev sonu origin değişmeyeceği için hiçbirinin tekrar atanmasına gerek yok.
+			// ************* alınan origin => x,y,z = 0,0,2 ***************
+			_counter = 0.0f;
+			_stage = 4;
+			_position_setpoint = Vector3f(NAN,NAN,NAN);
+			_velocity_setpoint = Vector3f(NAN,NAN,NAN);
+
+		}
+
+		break;
+	case 4://iniş
+		_position_setpoint(0) = _origin_x;
+		_position_setpoint(1) = _origin_y;
+		_position_setpoint(2) = _origin_z;
+
+		_counter = _counter + _counter_speed;
+
+		_yaw_setpoint = _yaw_speed * _counter * 3.141592653589793f / 180.f;
+		if(_counter >= 370.0f)
+		{
+		//In ve komut vermeyi kes
+
+			_stage = 5;
+		}
+
+
+		break;
+	case 5:
+		// publish the vehicle command
+		_publishVehicleCmdDoLand();
+		//Bitis
+		break;
+
+	default:
+		break;
+	}
+
+
+
+
+
+
+
+	return true;
+}
---
+++ src/lib/flight_tasks/tasks/Burkut/FlightTaskBurkut.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "FlightTask.hpp"
+#include <cmath>
+
+//obstacleAvoidance example
+#include <px4_platform_common/defines.h>
+#include <px4_platform_common/module_params.h>
+#include <commander/px4_custom_mode.h>
+#include <uORB/topics/vehicle_command.h>
+#include <uORB/PublicationQueued.hpp>
+#include <uORB/Publication.hpp>
+//#include <lib/hysteresis/hysteresis.h>
+
+
+
+#include <uORB/topics/vehicle_land_detected.h>
+#include <uORB/topics/position_setpoint_triplet.h>
+
+
+class FlightTaskBurkut : public FlightTask
+{
+
+public:
+	FlightTaskBurkut() = default;
+	virtual ~FlightTaskBurkut() = default;
+
+	bool update() override;
+	bool activate(vehicle_local_position_setpoint_s last_setpoint) override;
+
+private:
+	int _stage = 0;
+	float _origin_z = 0.0f;
+	float _origin_y = 0.0f;
+	float _origin_x = 0.0f;
+	float _origin_yaw = 0.0f;
+	float _counter = 0.0f;
+	float _radian_of_degree = 0.0f;
+	float _radius_of_chamber = 3.0f;
+	float _yaw_speed = 300.0f;
+	float _counter_speed = 0.01f;
+
+	int32_t _default_mpc_auto_mode = 1;
+protected:
+	DEFINE_PARAMETERS(
+					(ParamInt<px4::params::MPC_POS_MODE>) _param_mpc_auto_mode, //mode
+					(ParamFloat<px4::params::NAV_LOITER_RAD>) _param_yaw_speed
+				       )
+	uORB::PublicationQueued<vehicle_command_s> _pub_vehicle_command{ORB_ID(vehicle_command)};	/**< vehicle command do publication */
+
+	/**
+	 * Publishes vehicle command.
+	 */
+	void _publishVehicleCmdDoLand();
+	void _bodyToNedFrame(float xBody,float yBody,float yawBody);
+};
